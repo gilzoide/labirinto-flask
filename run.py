282be945d08ee1ff9864bdc3c9b1f9898dd90de0
gilzoide <gilzoide@gmail.com>
2017-02-03 10:29:58 -0200

    Testando o docker =]

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ import os
 modo = argv[1] if len(argv) > 1 else 'dev'
 assert modo in os.listdir(os.path.join('labirinto', 'instancias')), "Modo de execução deve ser 'dev' ou 'teste'"
 app = create_app(modo)
-app.run(debug=True, use_reloader=True)
+app.run(debug=True, use_reloader=True, host='0.0.0.0')
